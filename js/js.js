5288fa67d9cb6a26fb199b0c22a6fbc729d96719
Hayat <rahnamoonhayatullah@gmail.com>
2018-04-23 22:56:06 +0800

    update

---
+++ js/js.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.showcase-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.showcase-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.showcase-bnt', {
+    duration: 2000,
+    delay: 2000,
+    origin: 'bottom',
+});
+sr.reveal('#testimonial div', {
+    duration: 2000,
+    origin: 'bottom',
+});
+sr.reveal('.info-left', {
+    duration: 2000,
+    origin: 'left',
+    viewFactor: 0.2 // this show how many persont of the element viewed to make the animation
+});
+sr.reveal('.info-right', {
+    duration: 2000,
+    origin: 'right',
+    viewFactor: 0.2
+});
